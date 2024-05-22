60a2041f9af5cbd992647ff6be68638e0f19d06d
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2024-05-21 20:14:14 -0400

    setting up react-query

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,22 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Applayout from "./ui/AppLayout";
 import Bookings from "./pages/Bookings";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 6 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
