8726fe18595b72ee44691e5998f57aff6aca0293
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2024-06-21 15:03:43 -0400

    API sode filtering : filtering bookings

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -9,7 +9,7 @@ const StyledSelect = styled.select`
         ? "var(--color-grey-100)"
         : "var(--color-grey-300)"};
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-grey-0);
+  background-color: var(--color-grey-100);
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
