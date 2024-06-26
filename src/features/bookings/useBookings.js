379dfeb5faaadda8d04f79c3c835a8a73f99f7a9
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2024-06-26 14:24:06 -0400

    API-Side Pagination: Paginating Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -17,9 +17,12 @@ export function useBookings() {
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
 
+  // Pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const { isLoading, data, error } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
   const bookings = data?.data || [];
